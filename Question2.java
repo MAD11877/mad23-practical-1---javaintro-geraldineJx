ea90a3d666a653fb9766a2358e636bc75b67d2b0
Geraldine Koh <geraldine.jx@gmail.com>
2023-04-22 23:41:24 +0800

    2

--- Question2.java
+++ Question2.java
@@ -12,10 +12,10 @@ public class Question2
      
     Scanner in = new Scanner(System.in);
 
-    System.out.println("Enter your height in meters: ");
+    // System.out.println("Enter your height in meters: ");
     double height = in.nextDouble();
 
-    System.out.println("Enter your weight in kilograms: ");
+    // System.out.println("Enter your weight in kilograms: ");
     double weight = in.nextDouble();
 
     double bmi = weight / (height * height);
