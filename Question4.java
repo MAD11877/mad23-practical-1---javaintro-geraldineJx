fd74b008be950ff0cbdfb9c22c23fe99e0bbe4e9
Geraldine Koh <geraldine.jx@gmail.com>
2023-04-22 23:26:02 +0800

    Completed

--- Question4.java
+++ Question4.java
@@ -19,5 +19,18 @@ public class Question4
      
     Scanner in = new Scanner(System.in);
     
+    System.out.print("Enter the base of triangle: ");
+    int input = in.nextInt();
+
+    for (int i = input; i > 0; i--) 
+    {
+      for (int j = i; j > 0; j--) 
+      {
+          System.out.print("*");
+      }
+      System.out.println();
+    }
+  
+      in.close();
   }
 }
